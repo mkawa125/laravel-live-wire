0da6935afef6439345f32bb24dc9cfa7d68facae
Dahabu Saidi <dahabusaidi@gmail.com>
2022-02-23 10:43:35 +0300

    Reduce padding bottom

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -32,27 +32,27 @@
             </button> --}}
         </div>
         <nav class="text-white  pt-3">
-            <a href="index.html" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+            <a href="index.html" class="flex items-center active-nav-link text-white py-3 pl-6 nav-item">
                 <i class="fas fa-tachometer-alt mr-3"></i>
                 Dashboard
             </a>
-            <a href="blank.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="blank.html" class="flex items-center text-white opacity-75 hover:opacity-100  py-3 pl-6 nav-item">
                 <i class="fas fa-sticky-note mr-3"></i>
                 Blank Page
             </a>
-            <a href="tables.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="tables.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-3  pl-6 nav-item">
                 <i class="fas fa-table mr-3"></i>
                 Tables
             </a>
-            <a href="forms.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="forms.html" class="flex items-center text-white opacity-75 hover:opacity-100  py-3 pl-6 nav-item">
                 <i class="fas fa-align-left mr-3"></i>
                 Forms
             </a>
-            <a href="tabs.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="tabs.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-3 pl-6 nav-item">
                 <i class="fas fa-tablet-alt mr-3"></i>
                 Tabbed Content
             </a>
-            <a href="calendar.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="calendar.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-3 pl-6 nav-item">
                 <i class="fas fa-calendar mr-3"></i>
                 Calendar
             </a>
