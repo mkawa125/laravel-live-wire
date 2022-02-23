b34377893ae0698c2c0f2fa276586b6f2a6f018e
Dahabu Saidi <dahabusaidi@gmail.com>
2022-02-23 09:13:16 +0300

    Add tailwind scripts

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enventory Management System</title>
+    <title>Laravel Live Wire</title>
     <meta name="author" content="Dahabu Saidi Mkawa">
     <meta name="description" content=" This is the enventory management system ">
 
     <!-- Tailwind -->
     <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
         .font-family-karla { font-family: karla; }
